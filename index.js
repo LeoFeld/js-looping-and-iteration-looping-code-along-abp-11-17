83396dc037e5b37bd39048312b8604366015f35d
Leo Feld <leofredrickfeld@gmail.com>
2018-01-17 14:46:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+function tailsNeverFails() {
+   return Math.random() >= 0.5;
+}
+
+while(tailsNeverFails()) {
+  console.log(`you got tails ${tailsNeverFails} times!`);
+}
